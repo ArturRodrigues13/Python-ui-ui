9c39207e2feb77ff1e395027c13411ab9d15589e
Artur Rodrigues Santos <arturrodriguesfla13@gmail.com>
2025-03-17 20:14:26 -0300

    teste

--- Brincadeirinhas/Testes Aleatórios.py
+++ Brincadeirinhas/Testes Aleatórios.py
@@ -275,8 +275,7 @@ for x in myset:
 set1 = {"a", "b", "c"}
 set2 = {1, 2, 3}
 set3 = {"John", "Elena"}
-set4 = {"apple", "bananas", "cherry"}
+set4 = {"apple", "bananas", "cherr"}
 
 myset2 = set1.union(set2, set3, set4)
 print(myset2)
-
